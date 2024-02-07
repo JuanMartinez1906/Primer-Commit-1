959aad883dfa504486c6fa3ba7ec9d9770c97638
Juan Esteban <juanestebanmartinezbotero@gmail.com>
2024-02-07 15:24:14 -0500

    Buscador de Peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {'name': 'Juan Esteban Martinez'})
+    #return render(request, 'home.html', {'name': 'Juan Esteban Martinez'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title_icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies, 'name': 'Juan Esteban Martinez'})
 def about(request):
     return render(request, 'about.html')
