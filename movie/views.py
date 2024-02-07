a17d63a499ab8ab7e05936e96f34316a7e525d28
Juan Esteban <juanestebanmartinezbotero@gmail.com>
2024-02-07 12:14:03 -0500

    cambios en las urls

--- movie/views.py
+++ movie/views.py
@@ -4,8 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    #return HttpResponse('<h1>Welcome to Home Page</h1>')
-    #return render(request, 'home.html')
     return render(request, 'home.html', {'name': 'Juan Esteban Martinez'})
 def about(request):
-    return HttpResponse('<h1>Welcome to About Page</h1>')
+    return render(request, 'about.html')
